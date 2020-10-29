0cd5425e6832f39a80b50bb7af01178dbc4b8641
Khang Le Truong <khang.letruong@pexraytech.com>
2020-10-29 14:22:30 +0700

    Add more color palettes

--- Palettes.h
+++ Palettes.h
@@ -1,6 +1,8 @@
 #ifndef PALETTES_H
 #define PALETTES_H
 
+extern const int colormap_fusion[];
+extern const int colormap_glowbow[];
 extern const int colormap_rainbow[];
 extern const int colormap_grayscale[];
 extern const int colormap_ironblack[];
